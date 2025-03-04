ea43835eeaab570c941becb54e82cf1ec52133c0
xc2457-2457 <yao132466@gmail.com>
2025-03-04 14:35:48 +0800

    assignment

--- assignment/sketch.js
+++ assignment/sketch.js
@@ -4,22 +4,68 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let currentback=0
 
-
+let bcolor =['orange','aqua','gray','blue']
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  background(220);
-  fill(150,255,255);
-  rect(0,0,width,140);
-  fill(255,255,170);
-  triangle(-30,440,50,50,350,height);
   
+ 
+  
+
+  background(220);
+  fill(bcolor[currentback]);
+  rect(0,0,width,height);
   fill(255,255,120);
+  
   circle(mouseX,mouseY,40);
   noStroke();
+  fill(255,255,170);
+  triangle(-30,height,50,50,350,height);
+  triangle(-30,height,width/2,50,width,height);
+  triangle(width,height,width,50,350,height);
+  fill(155,200,100);
+  circle(width/2-1000,height+500,1900);
+  fill(155,220,100);
+  circle(width/2+1000,height+500,1900);
+  fill(155,230,100);
+  circle(width/2,height+500,2300);
+
+  
+  
+  
+  
+  
+}
+function keyPressed() {
+  if (key === 'c') {
+    // Code to run.
+    
+    currentback++;
+  }
+    if(currentback ===4){
+      currentback = 0;
+    }
+
+    
+    
+      
+
+  
+  
+
+  
+
+  
+  
+
+
+  
+
   
   
 }
