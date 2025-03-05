0b2072c84bc32552f1e4920a7c8a32c3b0d765a8
xc2457-2457 <yao132466@gmail.com>
2025-03-05 11:25:27 -0600

    assignment

--- assignment/sketch.js
+++ assignment/sketch.js
@@ -1,28 +1,31 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-let currentback = 0;
+// Interactive scene
+// Xinchen Yao
+// MAR 5
 
+
+let currentback = 0;
+//color array
 let bcolor = ['orange', 'aqua', 'gray', 'blue'];
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  
+  
 
 }
+let d = 40;
 
 function draw() {
 
 
-
+  // background
 
   background(220);
   fill(bcolor[currentback]);
   rect(0, 0, width, height);
   fill(255, 255, 120);
 
-  circle(mouseX, mouseY, 40);
+  circle(mouseX, mouseY, d);
   noStroke();
   fill(255, 255, 170);
   triangle(-30, height, 50, 50, 350, height);
@@ -34,6 +37,8 @@ function draw() {
   circle(width / 2 + 200, height + 100, 330);
   fill(155, 230, 100);
   circle(width / 2, height + 100, 400);
+  fill('black');
+  text('xinchen',550,300);
 
 
 
@@ -44,9 +49,23 @@ function draw() {
 function mousePressed() {
   print("aaa");
   if (mouseButton === CENTER) {
-    currentback++;// when the left mouse is clicked
+    currentback++;// when the center button is clicked
   }
   if (currentback === 4) {        //the "currentBack" variable changes
     currentback = 0;         //the "currentBack"will not count to 4,it will reset to 0
   }
+}
+// use a and d to change the size
+function keyPressed(){
+  if(key==='a'){
+    d = d-5;
+
+
+  }
+  if(key==='d'){
+    d = d+5;
+
+
+  }
+  
 }
