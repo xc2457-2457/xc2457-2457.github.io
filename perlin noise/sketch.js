0b1986dd30047143cd70a30b95c8925af7c250b2
xc2457-2457 <yao132466@gmail.com>
2025-03-11 12:48:39 +0800

    noise

--- perlin noise/sketch.js
+++ perlin noise/sketch.js
@@ -8,24 +8,37 @@
 
 
 function setup() {
-  createCanvas(400, 400);
-  generateTerrain();
+  createCanvas(windowWidth, windowHeight);
+  generateTerrain(frameCount*0.01);
 }
 
 
 
 function generateTerrain(){
-  let xoff = 0; 
+  let xoff = frameCount*0.03;
+  let peeky = height
+  let peekx;
   for (let x = 0; x < width; x++) {
-    let y = noise(xoff) * height; 
+    let y = noise(xoff) * height; //noise(xoff)=0-1 height increase through noise value decrease
     
     xoff += 0.01;
     line(x,y,x,height);
+    if(y<peeky){//find the highest point
+      peeky = y
+      peekx = x
+    } 
+    
   }
-
-
-
+  drawflag(peekx,peeky) 
 }
+function drawflag(x,y){
+  circle(x,y,30)
+  
+}
+
+
+
+
 function draw() {
   background(220);
   generateTerrain();
