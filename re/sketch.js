514160d58ee143e1cdaa646c576d677f1806a256
xc2457-2457 <yao132466@gmail.com>
2025-05-05 22:19:14 +0800

    11

--- re/sketch.js
+++ re/sketch.js
@@ -12,17 +12,23 @@ const NUM_ROWS = 3; const NUM_COLS = 5;
 
 function setup() {
   createCanvas(NUM_COLS * squareSize, NUM_ROWS * squareSize);
+  random11();
+  
+}
+
+function random11(){
   for (let y = 0; y < NUM_ROWS; y++) {
     grid[y] = [];
     for (let x = 0; x < NUM_COLS; x++) {
       grid[y][x] = random([0,255]);
-      let fillColor = grid[y][x];
-      fill(fillColor);
-      square(x * squareSize, y * squareSize, squareSize);
+      
     }
   }
+
   
 }
+
+
 
 function renderGrid() {
   // interpret the information in the 2D array, and draw
@@ -34,6 +40,17 @@ function renderGrid() {
       square(x * squareSize, y * squareSize, squareSize);
     }
   }
+
+  let greenx = getCurrentX();
+  let greeny = getCurrentY();
+
+  noStroke();
+  fill(0,255,0,50);
+  square(greenx*squareSize,greeny*squareSize,squareSize);
+  if (greeny > 0) square(greenx * squareSize, (greeny - 1) * squareSize, squareSize);
+  if (greeny < NUM_ROWS - 1) square(greenx * squareSize, (greeny + 1) * squareSize, squareSize);
+  if (greenx > 0) square((greenx - 1) * squareSize, greeny * squareSize, squareSize);
+  if (greenx < NUM_COLS - 1) square((greenx + 1) * squareSize, greeny * squareSize, squareSize);
 
   
 
@@ -83,12 +100,7 @@ function flip(x, y){
   else grid[y][x] = 0;
 }
 
-function green(x,y){
-  
-  fill(0,255,0,120)
-  
-  square(xx*squareSize,yy*squareSize,squareSize)
-}
+
 
 
 
@@ -96,16 +108,7 @@ function draw() {
   background(220);
   renderGrid();
 
-  let x = getCurrentX();
-  let y = getCurrentY();
-  green(x,y);
-    //sometimes: (depending on position) flip the neighbours
-  if(y > 0) green(x, y-1);  //NORTH 
-  if(x > 0) green(x-1, y);  //WEST
-  if(x < NUM_COLS-1) green(x+1, y); //EAST
-  if(y < NUM_ROWS-1) green(x, y+1);
-
-  stroke(0);
+  
 
 
   let grid00 = grid[0][0];
